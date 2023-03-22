1d07fde1d772c05d1f4b680674403cd5ee47669b
Kaito <hoangdai.d93@gmail.com>
2023-03-22 10:34:51 +0100

    first commit

---
+++ src/router/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+function index() {
+  return (
+    <Routes>
+      {/* <Route to='' element={}></Route> */}
+      {/* <Route to='' element={}></Route> */}
+      {/* <Route to='' element={}></Route> */}
+    </Routes>
+  )
+}
+
+export default index
